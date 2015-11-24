e591813baaf0b40b369ed4228fe9291e13a5900b
daesung-kim <saint.pablo94@gmail.com>
2015-11-24 21:39:51 +0900

    exam14

---
+++ src/main/java/exam/test14/Tire.java
@@ -0,0 +1,48 @@
+package exam.test14;
+
+import java.util.Date;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+public class Tire {
+	String maker;
+	Properties spec;
+	Date createDate;
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public Properties getSpec() {
+		return spec;
+	}
+
+	public void setSpec(Properties spec) {
+		this.spec = spec;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String toString() {
+		StringBuilder specInfo = new StringBuilder();
+		if(spec != null){
+			for(Entry<Object,Object> entry : spec.entrySet()){
+				specInfo.append(entry.getKey()+":"+entry.getValue()+",");
+			}
+		}
+		
+		return "[Tire : " + maker + " , " + specInfo.toString()
+				+ ((createDate != null) ? ("," + createDate.toString()) : "")
+				+ "]";
+	}
+}
---
+++ src/main/java/exam/test14/TireFactoryBean.java
@@ -0,0 +1,77 @@
+package exam.test14;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+
+public class TireFactoryBean extends AbstractFactoryBean<Tire> {
+	
+	String maker;
+	
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public Tire createTire(String maker) {
+		if (maker.equals("Hankook")) {
+			return createHankookTire();
+		} else {
+			return createKumhoTire();
+		}
+	}
+
+	private Tire createKumhoTire() {
+		Tire tire = new Tire();
+		tire.setMaker("Kumho");
+
+		Properties specProp = new Properties();
+		specProp.setProperty("width", "185");
+		specProp.setProperty("ratio", "75");
+		specProp.setProperty("rim.diameter", "16");
+		tire.setSpec(specProp);
+
+		SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		try {
+			tire.setCreateDate(dataFormat.parse("2014-03-01"));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return tire;
+	}
+
+	private Tire createHankookTire() {
+		Tire tire = new Tire();
+		tire.setMaker("Hankook");
+		Properties specProp = new Properties();
+		specProp.setProperty("width", "205");
+		specProp.setProperty("ratio", "65");
+		specProp.setProperty("rim.diameter", "14");
+		tire.setSpec(specProp);
+
+		SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			tire.setCreateDate(dataFormat.parse("2014-05-05"));
+		} catch (Exception e) {
+		}
+
+		return tire;
+	}
+
+	@Override
+	protected Tire createInstance() throws Exception {
+		if (maker.equals("Hankook")) {
+			return createHankookTire();
+		} else {
+			return createKumhoTire();
+		}
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return exam.test13.Tire.class;
+	}
+}
