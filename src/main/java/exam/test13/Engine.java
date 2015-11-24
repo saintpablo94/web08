3358f8eeea972c9fdf94a6553f89c2a669a58549
daesung-kim <saint.pablo94@gmail.com>
2015-11-24 21:31:47 +0900

    exam13

---
+++ src/main/java/exam/test13/Engine.java
@@ -0,0 +1,34 @@
+package exam.test13;
+
+public class Engine {
+	String maker;
+	int cc;
+
+	public Engine() {
+	}
+
+	public Engine(String maker) {
+		this.maker = maker;
+	}
+
+	public String getMaker() {
+		return maker;
+	}
+
+	public void setMaker(String maker) {
+		this.maker = maker;
+	}
+
+	public int getCc() {
+		return cc;
+	}
+
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+
+	public String toString() {
+		return "[Engine : " + maker + " , " + cc + "]";
+	}
+
+}
